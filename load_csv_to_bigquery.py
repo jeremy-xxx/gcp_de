53ebff02622ba132971079280e92abb55decc5a4
jeremy <jeremygreats@gmail.com>
2023-11-17 10:40:24 -0500

    load_csv_to_bigquery script refactor

--- load_csv_to_bigquery.py
+++ load_csv_to_bigquery.py
@@ -1,85 +1,89 @@
-from google.cloud import bigquery
 import os
 import glob
+import pandas as pd
+from google.cloud import bigquery
 
-# Initialize a BigQuery client to interact with the service
+# Initialize a BigQuery client to interact with the Google BigQuery service.
+# This requires having the GOOGLE_APPLICATION_CREDENTIALS environment variable set.
 client = bigquery.Client()
 
-# Assign the dataset name where you want to create the tables. This dataset should already been created in bigquery
-dataset_name = 'sensor_data'
 
-# Find all CSV files in the data directory and its subdirectories
-data_folder = 'data'
-csv_files = glob.glob(f'{data_folder}/**/dataset_*.csv', recursive=True)
-
-# Group CSV files by the file name (without the path and extension)
-csv_files_grouped = {}
-for csv_file in csv_files:
-    base_name = os.path.basename(csv_file)
-    table_name = f'ods_{os.path.splitext(base_name)[0]}'
-    if table_name not in csv_files_grouped:
-        csv_files_grouped[table_name] = []
-    csv_files_grouped[table_name].append(csv_file)
-
-def create_table_and_load_data(dataset_name, table_name, csv_files):
+def read_csv_file(csv_file, subfolder_name):
     """
-    Creates a new BigQuery table and loads data from the specified CSV files.
-    If a table with the same name already exists, it overwrites it.
-    Also creates a partitioned table based on a timestamp column.
+    Reads a CSV file into a pandas DataFrame and adds a 'data_source' column.
 
-    Args:
-    dataset_name (str): The dataset name in BigQuery.
-    group_info (tuple): A tuple containing paths to CSV files, table name, and schema.
+    Parameters:
+    - csv_file: The path to the CSV file.
+    - subfolder_name: The name of the subfolder where the CSV file is located, which will be used as the value for the 'data_source' column.
+
+    Returns:
+    - A pandas DataFrame with the CSV data and an additional 'data_source' column.
     """
-    table_id = f"{client.project}.{dataset_name}.{table_name}"
-    temp_table_id = f"{table_id}_temp"
+    df = pd.read_csv(csv_file)
+    df['data_source'] = subfolder_name
+    return df
 
-    # Create a temporary table
-    temp_table = bigquery.Table(temp_table_id)
-    client.create_table(temp_table, exists_ok=True)
 
-    # Load data from each CSV file into the temporary table
+def load_dataframe_to_bigquery(df, dataset_name, table_name):
+    """
+    Loads a pandas DataFrame to a BigQuery table.
+
+    Parameters:
+    - df: The pandas DataFrame to load.
+    - dataset_name: The name of the dataset in BigQuery where the table will be created.
+    - table_name: The name of the BigQuery table where the DataFrame will be loaded.
+
+    The function waits for the job to complete and prints the number of rows loaded.
+    """
+    table_id = f"{client.project}.{dataset_name}.{table_name}_temp"
+    job_config = bigquery.LoadJobConfig(autodetect=True)
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result()  # Waits for the job to complete
+    print(f"Loaded {job.output_rows} rows into {table_id}")
+
+
+def find_csv_files(data_folder):
+    """
+    Finds all CSV files in the specified data folder and its subdirectories.
+
+    Parameters:
+    - data_folder: The root directory to search for CSV files.
+
+    Returns:
+    - A list of paths to the CSV files.
+    """
+    return glob.glob(f'{data_folder}/**/dataset_*.csv', recursive=True)
+
+
+def load_csv_to_bigquery(csv_file, dataset_name):
+    """
+    Processes a single CSV file: reads it into a DataFrame, adds a 'data_source' column, and loads it to BigQuery.
+
+    Parameters:
+    - csv_file: The path to the CSV file.
+    - dataset_name: The name of the dataset in BigQuery where the table will be created.
+
+    The table name is derived from the CSV file name.
+    """
+    subfolder_name = os.path.basename(os.path.dirname(csv_file))
+    table_name = 'ods_' + os.path.splitext(os.path.basename(csv_file))[0]
+    df = read_csv_file(csv_file, subfolder_name)
+    load_dataframe_to_bigquery(df, dataset_name, table_name)
+
+
+def main():
+    """
+    The main entry point of the script.
+    It sets up the dataset name, finds all CSV files in the 'data' folder, and loads each one into BigQuery.
+    """
+    dataset_name = 'sensor_data'  # Name of the dataset in BigQuery.
+    data_folder = 'data'  # Directory containing CSV files.
+    csv_files = find_csv_files(data_folder)  # List of CSV file paths.
+
     for csv_file in csv_files:
-        job_config = bigquery.LoadJobConfig(
-            source_format=bigquery.SourceFormat.CSV,
-            skip_leading_rows=1,
-            autodetect=True
-        )
-        with open(csv_file, "rb") as source_file:
-            job = client.load_table_from_file(source_file, temp_table_id, job_config=job_config)
-        job.result()
-        print(f"Loaded {job.output_rows} rows into {temp_table_id} from {csv_file}")
+        load_csv_to_bigquery(csv_file, dataset_name)
 
-    # Transform and load data into the final partitioned table
-    job_config = bigquery.QueryJobConfig()
-    sql = f"""
-    CREATE OR REPLACE TABLE `{table_id}`
-    PARTITION BY TIMESTAMP_TRUNC(timestamp_formatted, DAY) AS
-    SELECT *, TIMESTAMP_SECONDS(CAST(timestamp AS INT64)) as timestamp_formatted
-    FROM `{temp_table_id}`;
-    """
-    job = client.query(sql, job_config=job_config)
-    job.result()
+    print("All tables created and data loaded.")
 
-    # Clean up: delete the temporary table
-    client.delete_table(temp_table_id, not_found_ok=True)
-    print(f"Temporary table {temp_table_id} deleted.")
-
-    print(f"Data transformed and loaded into partitioned table {table_id}.")
-
-# Iterate over the grouped CSV files and load them into BigQuery
-for table_name, csv_files in csv_files_grouped.items():
-    create_table_and_load_data(dataset_name, table_name, csv_files)
-
-print("All tables created and data loaded.")
-
-# Sample query to select data from a partitioned table within a date range
-# Replace `your_project.your_dataset.your_table` with your actual table path
-sample_query = """
-SELECT *
-FROM `your_project.your_dataset.your_table`
-WHERE _PARTITIONTIME BETWEEN TIMESTAMP('2023-01-01') AND TIMESTAMP('2023-01-31')
-"""
-
-
-
+if __name__ == "__main__":
+    main()
