aad4fd9280b61b9350b932c297f6552e896c0c3e
jeremy <jeremygreats@gmail.com>
2023-11-18 22:40:14 -0500

    limit the resource consuming

--- cloud_shell_scripts/run_dataflow_jobs.sh
+++ cloud_shell_scripts/run_dataflow_jobs.sh
@@ -3,25 +3,25 @@ source ./config.sh
 
 gcloud dataflow jobs run "${PUBSUB_TOPIC1}_to_bq_job" \
     --gcs-location "gs://dataflow-templates-${REGION}/latest/PubSub_to_BigQuery" \
-    --region $REGION \
+    --region $REGION --max-workers 1 --num-workers 1 \
     --staging-location "gs://${BUCKET_NAME}/temp" \
     --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC1}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME1}"
 
 gcloud dataflow jobs run "${PUBSUB_TOPIC2}_to_bq_job" \
     --gcs-location "gs://dataflow-templates-${REGION}/latest/PubSub_to_BigQuery" \
-    --region $REGION \
+    --region $REGION --max-workers 1 --num-workers 1 \
     --staging-location "gs://${BUCKET_NAME}/temp" \
     --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC2}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME2}"
 
 gcloud dataflow jobs run "${PUBSUB_TOPIC3}_to_bq_job" \
     --gcs-location "gs://dataflow-templates-${REGION}/latest/PubSub_to_BigQuery" \
-    --region $REGION \
+    --region $REGION --max-workers 1 --num-workers 1 \
     --staging-location "gs://${BUCKET_NAME}/temp" \
     --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC3}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME3}"
 
 gcloud dataflow jobs run "${PUBSUB_TOPIC1}_to_cs_job" \
     --gcs-location "gs://dataflow-templates-${REGION}/latest/Cloud_PubSub_to_GCS_Text" \
-    --region ${REGION} \
+    --region ${REGION} --max-workers 1 --num-workers 1 \
     --staging-location "gs://${BUCKET_NAME}/temp" \
     --parameters \
 inputTopic=projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC1},\
@@ -31,7 +31,7 @@ outputFilenameSuffix=.txt
 
 gcloud dataflow jobs run "${PUBSUB_TOPIC2}_to_cs_job" \
     --gcs-location "gs://dataflow-templates-${REGION}/latest/Cloud_PubSub_to_GCS_Text" \
-    --region ${REGION} \
+    --region ${REGION} --max-workers 1 --num-workers 1 \
     --staging-location "gs://${BUCKET_NAME}/temp" \
     --parameters \
 inputTopic=projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC2},\
@@ -41,7 +41,7 @@ outputFilenameSuffix=.txt
 
 gcloud dataflow jobs run "${PUBSUB_TOPIC3}_to_cs_job" \
     --gcs-location "gs://dataflow-templates-${REGION}/latest/Cloud_PubSub_to_GCS_Text" \
-    --region ${REGION} \
+    --region ${REGION} --max-workers 1 --num-workers 1 \
     --staging-location "gs://${BUCKET_NAME}/temp" \
     --parameters \
 inputTopic=projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC3},\
