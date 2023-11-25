dca3e26381e70d7d4ad3c66eb346785809a3eefd
jeremy <jeremygreats@gmail.com>
2023-11-24 23:42:50 -0500

    more topics

--- cloud_shell_scripts/create_cloud_storage_sub.sh
+++ cloud_shell_scripts/create_cloud_storage_sub.sh
@@ -11,4 +11,24 @@ gcloud pubsub subscriptions create sub_to_cloud_storage \
     --cloud-storage-output-format=text \
     --cloud-storage-write-metadata
 
-# https://cloud.google.com/pubsub/docs/create-cloudstorage-subscription?hl=zh-cn#project-permissions
+gcloud pubsub subscriptions create sub_to_cloud_storage \
+    --topic=$PUBSUB_TOPIC2 \
+    --cloud-storage-bucket=$BUCKET_NAME \
+    --cloud-storage-file-prefix=${PUBSUB_TOPIC2}- \
+    --cloud-storage-file-suffix=.txt \
+    --cloud-storage-max-bytes=1MB \
+    --cloud-storage-max-duration=1m \
+    --cloud-storage-output-format=text \
+    --cloud-storage-write-metadata
+
+gcloud pubsub subscriptions create sub_to_cloud_storage \
+    --topic=$PUBSUB_TOPIC3 \
+    --cloud-storage-bucket=$BUCKET_NAME \
+    --cloud-storage-file-prefix=${PUBSUB_TOPIC3}- \
+    --cloud-storage-file-suffix=.txt \
+    --cloud-storage-max-bytes=1MB \
+    --cloud-storage-max-duration=1m \
+    --cloud-storage-output-format=text \
+    --cloud-storage-write-metadata
+
+# https://cloud.google.com/pubsub/docs/create-cloudstorage-subscription
