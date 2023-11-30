5f16676b181244e78f4e67c39e2933bdd32f191a
jeremy <jeremygreats@gmail.com>
2023-11-30 18:03:37 -0500

    cloud scheduler for triggering cloud function built

---
+++ cloud_shell_scripts/create_cloud_scheduler.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ./config.sh
+
+gcloud scheduler jobs create http execute_sql \
+    --schedule="0 1 * * *" \
+    -uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/execute_sql" \
+    --http-method=GET \
+    --time-zone="America/Chicago"
+
+# test: gcloud scheduler jobs run execute_sql
