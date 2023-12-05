6375eb46b2d92f219bc3c1784f8a141cb0adf66c
jeremy <jeremygreats@gmail.com>
2023-12-04 21:25:17 -0500

    keep one job

--- cloud_shell_scripts/run_dataflow_jobs.sh
+++ cloud_shell_scripts/run_dataflow_jobs.sh
@@ -7,16 +7,16 @@ gcloud dataflow jobs run "${PUBSUB_TOPIC1}_to_bq_job" \
     --staging-location "gs://${BUCKET_NAME}/temp" \
     --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC1}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME1}"
 
-gcloud dataflow jobs run "${PUBSUB_TOPIC2}_to_bq_job" \
-    --gcs-location "gs://dataflow-templates-${REGION}/latest/PubSub_to_BigQuery" \
-    --region $REGION --max-workers 1 --num-workers 1 \
-    --staging-location "gs://${BUCKET_NAME}/temp" \
-    --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC2}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME2}"
-
-gcloud dataflow jobs run "${PUBSUB_TOPIC3}_to_bq_job" \
-    --gcs-location "gs://dataflow-templates-${REGION}/latest/PubSub_to_BigQuery" \
-    --region $REGION --max-workers 1 --num-workers 1 \
-    --staging-location "gs://${BUCKET_NAME}/temp" \
-    --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC3}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME3}"
+#gcloud dataflow jobs run "${PUBSUB_TOPIC2}_to_bq_job" \
+#    --gcs-location "gs://dataflow-templates-${REGION}/latest/PubSub_to_BigQuery" \
+#    --region $REGION --max-workers 1 --num-workers 1 \
+#    --staging-location "gs://${BUCKET_NAME}/temp" \
+#    --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC2}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME2}"
+#
+#gcloud dataflow jobs run "${PUBSUB_TOPIC3}_to_bq_job" \
+#    --gcs-location "gs://dataflow-templates-${REGION}/latest/PubSub_to_BigQuery" \
+#    --region $REGION --max-workers 1 --num-workers 1 \
+#    --staging-location "gs://${BUCKET_NAME}/temp" \
+#    --parameters inputTopic="projects/${PROJECT_ID}/topics/${PUBSUB_TOPIC3}",outputTableSpec="${PROJECT_ID}:${DATASET_NAME}.${BQ_TABLE_NAME3}"
 
 # ref https://cloud.google.com/dataflow/docs/guides/templates/provided-templates
